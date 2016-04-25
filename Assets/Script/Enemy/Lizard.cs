8a72f19653e63ad0882f34a974138f2824e63a52
Jae Hyun Yoo <fkkcloud@gmail.com>
2016-04-24 23:16:59 -0700

    update game

---
+++ Assets/Script/Enemy/Lizard.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Attacker))]
+public class Lizard : MonoBehaviour {
+
+	private Animator anim;
+	private Attacker attacker;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		attacker = GetComponent<Attacker> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+
+		GameObject obj = col.gameObject;
+
+		// if its not defender, abort.
+		if (!obj.GetComponent<Defender> ()) {
+			return;
+		}
+
+		if (anim) {
+			anim.SetBool ("IsAttacking", true);
+			attacker.Attack (obj);
+		}
+
+		Debug.Log (name + " Collided with " + col);
+	}
+}
